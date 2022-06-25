40ce7f0ec4d5c0a593560de68c3ef4dc9579b063
Ankit Kaushal <kaushalankit101010@gmail.com>
2022-06-25 21:28:43 +0530

    cred & AWS upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
+const AWS = require("aws-sdk");
 const cors = require("cors");
 const path = require("path");
 
@@ -27,6 +29,70 @@ const port = process.env.PORT || 9876;
 app.use(express.static(path.join(__dirname, "/app/dist")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/app/dist/index.html"));
+});
+
+app.get("/phone", (req, res) => {
+  console.log("Message = " + req.query.message);
+  console.log("Number = " + req.query.number);
+  console.log("Subject = " + req.query.subject, "\n");
+  var params = {
+    Message: req.query.message,
+    PhoneNumber: "+" + req.query.number,
+    MessageAttributes: {
+      "AWS.SNS.SMS.SenderID": {
+        DataType: "String",
+        StringValue: req.query.subject,
+      },
+    },
+  };
+  let sns = new AWS.SNS({ apiVersion: "2010-03-31" });
+
+  sns.publish(params, (err, data) => {
+    if (err) {
+      res.end(JSON.stringify({ Error: err }));
+    } else {
+      res.end(JSON.stringify({ MessageID: data.MessageId }));
+    }
+  });
+});
+
+app.get("/mail", async (req, res) => {
+  const email = process.env.FROM_EMAIL;
+  const ses = new AWS.SES();
+
+  console.log(email);
+
+  try {
+    const params = {
+      Source: email,
+      Destination: {
+        ToAddresses: [
+          email,
+          "alexa.aiboost@gmail.com",
+          "founder.aiboost@gmail.com",
+        ],
+      },
+      Message: {
+        Subject: { Charset: "UTF-8", Data: `alert from TalentShip` },
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: `<h1>this is an alert from TalentShip </h1>`,
+          },
+        },
+      },
+    };
+    
+    await ses.sendEmail(params, (err, data) => {
+      if (err) {
+        res.end(JSON.stringify({ Error: err }));
+      } else {
+        res.end(JSON.stringify({ MessageID: data.MessageId }));
+      }
+    });
+  } catch (error) {
+    res.end(JSON.stringify({ Error: error }));
+  }
 });
 
 app.listen(port, (success, error) => {
